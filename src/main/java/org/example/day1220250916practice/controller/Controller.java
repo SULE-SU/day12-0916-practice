eb00e15074548555aea116ae3e67837e6f5043d0
SULE <sule1@oocl.com>
2025-09-16 11:44:26 +0800

    refactor: implement Todo controller and refactor package structure

---
+++ src/main/java/org/example/day1220250916practice/controller/Controller.java
@@ -0,0 +1,26 @@
+package org.example.day1220250916practice.controller;
+
+import org.example.day1220250916practice.entity.Todo;
+import org.example.day1220250916practice.repository.TodoRepository;
+import org.example.day1220250916practice.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class Controller {
+
+    private final TodoService todoService;
+
+    public Controller(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    List<Todo> index() {
+        return todoService.index();
+    }
+}
