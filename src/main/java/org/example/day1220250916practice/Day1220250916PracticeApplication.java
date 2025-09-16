0df2a72eacdbbe4420fefbef79988f3c819c8228
SULE <sule1@oocl.com>
2025-09-16 10:39:49 +0800

    init project

---
+++ src/main/java/org/example/day1220250916practice/Day1220250916PracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.day1220250916practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1220250916PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1220250916PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/day1220250916practice/Day1220250916PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day1220250916practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1220250916PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
